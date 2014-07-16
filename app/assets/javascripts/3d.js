bdd9df99f737d1fab74082c80bc64918c2b32d54
Phil Pirozhkov <pirjsuka@gmail.com>
2014-07-16 17:34:43 +0400

    clicking

--- app/assets/javascripts/3d.js
+++ app/assets/javascripts/3d.js
@@ -2,21 +2,28 @@
 // if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var container, stats;
-var camera, scene, renderer;
+var camera, scene, renderer, projector;
 var dae;
+var ground;
 
 var loader = new THREE.ColladaLoader();
 loader.options.convertUpAxis = true;
-loader.load( '/models/model.dae', function ( collada ) {
+loader.load( '/models/buildings.dae', function ( collada ) {
   dae = collada.scene;
 
-  dae.scale.x = dae.scale.y = dae.scale.z = 0.2;
+  deepComputeBoundingBox(dae);
+
+  dae.scale.x = dae.scale.y = dae.scale.z = 0.01;
   dae.updateMatrix();
 
   init();
   render();
-  // requestAnimationFrame(render);
 });
+
+function deepComputeBoundingBox(object) {
+  object.children.forEach(deepComputeBoundingBox);
+  if(object.geometry) object.geometry.computeBoundingBox();
+}
 
 function init() {
   container = document.createElement( 'div' );
@@ -25,11 +32,12 @@ function init() {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
 
+  projector = new THREE.Projector();
+
   var aspect = window.innerWidth / window.innerHeight;
   var d = 20;
   camera = new THREE.OrthographicCamera( - d * aspect, d * aspect, d, - d, 1, 1000 );
   camera.position.set( 0, 20, 20 );
-  // camera.rotation.order = 'YXZ';
   camera.rotation.y = Math.PI / 3;
   camera.rotation.x = Math.atan( - 1 / Math.sqrt( 2 ) );
 
@@ -68,8 +76,17 @@ function init() {
   light.position.set( 0, 50, 50 );
   scene.add( light );
 
+  light = new THREE.AmbientLight( 0x404040 );
+  scene.add( light );
+
   // Axes
   scene.add( new THREE.AxisHelper( 40 ) );
+
+  var plane_geometry = new THREE.PlaneGeometry( 100, 100 );
+  var plane_material = new THREE.MeshBasicMaterial( {color: 0x6666aa, side: THREE.DoubleSide} );
+  ground = new THREE.Mesh(plane_geometry, plane_material);
+  ground.rotation.x = Math.PI / 2;
+  scene.add(ground);
 
   container.appendChild( renderer.domElement );
 
@@ -77,9 +94,34 @@ function init() {
   stats.domElement.style.position = 'absolute';
   stats.domElement.style.top = '0px';
   container.appendChild( stats.domElement );
+
+  container.addEventListener( 'dblclick', onDocumentMouseDown, false );
 }
 
 function render() {
   renderer.render( scene, camera );
   stats.update();
+}
+
+function onDocumentMouseDown( event ) {
+  event.preventDefault();
+  // var objects = dae.children[0].children.map(function(el) { return el.children; } ).reduce(function(acc, children) { return acc.concat(children); });
+  var objects = dae.children[0].children.reduce(function(acc, children) { return acc.concat(children); }, []);
+
+  var mouse3D = new THREE.Vector3(( event.clientX / window.innerWidth ) * 2 - 1, -( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
+  var raycaster = projector.pickingRay( mouse3D.clone(), camera );
+  var intersects = raycaster.intersectObjects(objects);
+  console.log(intersects);
+
+  if(intersects.length > 0) {
+    var intersect = intersects[0];
+    var color = Math.random() * 0xffffff;
+    var material = new THREE.MeshBasicMaterial( { color: color } );
+    // if(intersect.object.material.materials)
+    //   intersect.object.material.materials.forEach(function(material) { material.color.setHex(color); });
+    // else
+      intersect.object.material = material; //.color.setHex(color);
+  }
+
+  render();
 }
